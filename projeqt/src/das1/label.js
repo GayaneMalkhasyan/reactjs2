a60f57ae36bb07c2244fddbcc42e0d8c764aa433
GayaneMalkhasyan <gayanemalkhasyan06@gmail.com>
2022-10-06 18:46:04 +0300

    now projeqt

---
+++ projeqt/src/das1/label.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./style.css";
+
+//1 Ֆունկցիոնալ component
+
+// function HieveryBody(){
+//     return <h1>Hi Every Body</h1>
+// }
+// export default HieveryBody
+
+//2 class component
+
+// export default class ByEveryBody extends React.Component{
+//     render(){
+//         return <h1>By Every Body</h1>
+//     }
+// }
+
+
+//3 Ֆունկցիոնալ component  karch grelu dzev
+
+// export default function YeY(){
+//     return <h1>YeY</h1>
+// }
